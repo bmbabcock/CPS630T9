06c5e87202673a3bf22b7387057cea306f314446
Byron Babcock <bmbabcock@gmail.com>
2012-03-07 14:01:25 -0500

    Added dateCreated to person class.  May need to be changed depending of flow of information.  Also needs error checking for set date to ensure the value is a date.

--- Models/person.class.php
+++ Models/person.class.php
@@ -4,18 +4,29 @@
 *	information	that is unique for a person.	*
 *												*
 *	Author: Byron Babcock						*
-*	Date: 06/03/2012							*
+*	Date: 07/03/2012							*
 ************************************************/
 class Person extends Model{
-	/************************************
-	*	Constructs the	Perons object.	*
-	*									*
-	*	$row:	id - the databse id		*
-	*			fbid - the id from fb	*
-	*			name - the name from fb *
-	************************************/
+	private $dateCreated;
+	/****************************************
+	*	Constructs the	Perons object.		*
+	*										*
+	*	$row:	id - the databse id			*
+	*			fbid - the id from fb		*
+	*			name - the name from fb 	*
+	*			dateCreated - the date the	*
+						account registered	*
+	****************************************/
 	function __construct($row){
 		parent::__construct($row);
+		$this->dateCreated = $row['dateCreated'];
+	}
+	//TODO: Add error checking
+	function setDateCreated($dateTime){
+		$this-> dateCreated = $dateTime;
+	}
+	function getDateCreated(){
+		return $this->DateCreated;
 	}
 }
 ?>
