bf5e77eab2c3806ab99bb3206be5f5a77912b8c7
Byron Babcock <bbabcock@scs.ryerson.ca>
2012-03-09 13:02:35 -0500

    Added constructor to controller class."

--- Controllers/controller.class.php
+++ Controllers/controller.class.php
@@ -1,8 +1,17 @@
 <?php>
 
 abstract class Controller(){
+	private $view;
+	private $arguments;
+
+	function __construct($row){
+		$arguments = $row;
+	}
+		
+
 	abstract function getData();
 	function render(){
 		
 	}
 }
+<?>
