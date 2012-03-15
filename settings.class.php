111f2f00d27f714d4fea00f05b9fd9868f849bb3
Byron Babcock <bmbabcock@gmail.com>
2012-03-15 15:49:08 -0400

    Updated settings file to include fields for database information.

--- settings.class.php
+++ settings.class.php
@@ -2,4 +2,9 @@
 
 static class Settings{
 	public static const ROOT_DIR = //Put root directory here;
+	public static const DB_SERVER = //Put database server here;
+	public static const DB_USER = //Put database user name here;
+	public static const DB_PASSWORD = //Put database user password here;
+	public static const DB_NAME = //Put database name here;
 }
+<?>
