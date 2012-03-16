113c480c56e7e6c5bd4ba98556d9205488bf0f8f
Byron Babcock <bmbabcock@gmail.com>
2012-03-16 13:14:09 -0400

    Added authorization view and updated settings to store required values.  Please do not commit any settings files with stored values as the repo is public and certain values must be kept secret.

--- settings.class.php
+++ settings.class.php
@@ -6,5 +6,8 @@ static class Settings{
 	public static const DB_USER = //Put database user name here;
 	public static const DB_PASSWORD = //Put database user password here;
 	public static const DB_NAME = //Put database name here;
+	public static const APP_ID = //Put app id here
+	public static const APP_SECRET = //Put app secret here
+	public static const URL = //put URL here
 }
 <?>
