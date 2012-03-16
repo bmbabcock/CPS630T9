131189f03a548db3e9850a410b38aac9b5d78ec8
Byron Babcock <bmbabcock@gmail.com>
2012-03-16 12:27:20 -0400

    Finished controller base class.

--- Controllers/controller.class.php
+++ Controllers/controller.class.php
@@ -3,15 +3,17 @@
 abstract class Controller(){
 	private $view;
 	private $arguments;
+	private $data;
 
 	function __construct($row){
 		$arguments = $row;
 	}
 		
 
-	abstract function getData();
+	abstract function loadData();
 	function render(){
-		
+		expand($this->data);
+		include(Settings::ROOT_DIR . 'Views\\' . $view);
 	}
 }
 <?>
