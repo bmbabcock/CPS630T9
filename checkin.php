2a015c40654421e54f404147ff8b39f487b83ff2
Byron Babcock <bmbabcock@gmail.com>
2012-04-03 11:41:04 -0400

    Minor modifications to checkin.php for testing

--- checkin.php
+++ checkin.php
@@ -9,6 +9,8 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 curl_setopt($ch, CURLOPT_CAINFO, getcwd() . "/CACertificates/VerisignClass3PublicPrimaryCertificationAuthority.crt");
 $curl_handle=curl_init();
 curl_setopt($curl_handle,CURLOPT_URL,'https://graph.facebook.com/' + $id + '/checkins?access_token=' + $access_token);
+
+curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
 curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
 $buffer = curl_exec($curl_handle);
 curl_close($curl_handle);
