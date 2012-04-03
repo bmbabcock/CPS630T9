403c6818076047dfe63e20a1e7f6b3850b377691
Byron Babcock <bmbabcock@gmail.com>
2012-04-03 12:16:15 -0400

    Fixed mistake in checkin.php

--- checkin.php
+++ checkin.php
@@ -3,7 +3,7 @@
 $id = $_GET["id"];
 $access_token = $_GET["access_token"];
 $curl_handle=curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($$curl_handle, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($curl_handle,CURLOPT_URL,'https://graph.facebook.com/checkins?access_token=' . $access_token);
 curl_exec($curl_handle);
 curl_close($curl_handle);
