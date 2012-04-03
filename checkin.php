69e8f1c037f14db7f05fb7ee7619e516bc8b23a5
Byron Babcock <bmbabcock@gmail.com>
2012-04-03 12:03:59 -0400

    Minor modifications to checkin.php for testing

--- checkin.php
+++ checkin.php
@@ -7,8 +7,6 @@ echo ('id: ' . $id);
 $curl_handle=curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($curl_handle,CURLOPT_URL,'https://graph.facebook.com/checkins?access_token=' . $access_token);
-
-curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
 curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
 $buffer = curl_exec($curl_handle);
 curl_close($curl_handle);
